1e54af746052ccd2579790f703bbe387f3226654
RexSteele <rexralphsteele@gmail.com>
2020-04-26 14:23:24 -0600

    IEEE manufacturer assignment CSV and function to compare given MAC Addr with CSV

--- scanBlueberry.py
+++ scanBlueberry.py
@@ -1,4 +1,5 @@
 import bluetooth
+import pandas as pd
 import printBlueberry as printB
 
 #Scans all nearby bluetooth devices, return list of MACS and Device Names(if available). Devices must be in discoverable mode. Does not work with low power Bluetooth
@@ -17,13 +18,13 @@ def scanAllServices():
 
     printB.printServices(results)
 
-def scanOneServices(addr):
+def scanOneService(addr):
     results = bluetooth.find_service(name = None, uuid = None, address = addr)
 
     printB.printServices(results)
 
 #Scan services of all available bluetooth devices in range based on uuid value. Second arg for one device only
-def scanUUIDServices(uuidVal, addr = None):
+def scanUUIDService(uuidVal, addr = None):
     service_matches = bluetooth.find_service(uuid = uuidVal, bdaddr = addr)
 
     if len(service_matches == 0):
@@ -32,10 +33,20 @@ def scanUUIDServices(uuidVal, addr = None):
         printB.printServices(service_matches)
 
 #Scan services of all available bluetooth devices in range based on name. Second arg for one device only
-def scanNameServices(nameVal, addr = None):
+def scanNameService(nameVal, addr = None):
     service_matches = bluetooth.find_service(name = nameVal, bdaddr = addr)
 
     if len(service_matches == 0):
         printf(f'No services matching {uuidVal} found.')
     else:
         printB.printServices(service_matches)
+
+#Compare given macAddr with current IEEE assignments
+def getMan(macAddr):
+    tempAddr = macAddr.replace(':', '')
+    file = pd.read_csv("manufacturerOUI.csv")
+    df = file[['Assignment', 'Organization Name']]
+
+    for index, data in df.iterrows():
+        if tempAddr.upper().startswith(data['Assignment']):
+            print(data['Organization Name'])
