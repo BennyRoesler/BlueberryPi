89a2c3105e7948501e6dacdfcd376794f7876ba9
Benny <Benny.Roesler@colostate.edu>
2020-04-27 22:40:45 -0600

    Created continous scan function and timeout handler function

--- scanBlueberry.py
+++ scanBlueberry.py
@@ -1,6 +1,9 @@
-import bluetooth
+import bluetooth #pybluez
 import pandas as pd
 import printBlueberry as printB
+import csv
+import prettytable
+import signal
 
 #Scans all nearby bluetooth devices, return list of MACS and Device Names(if available). Devices must be in discoverable mode. Does not work with low power Bluetooth
 def scanAll():
@@ -28,7 +31,7 @@ def scanUUIDService(uuidVal, addr = None):
     service_matches = bluetooth.find_service(uuid = uuidVal, bdaddr = addr)
 
     if len(service_matches == 0):
-        printf(f'No services matching {uuidVal} found.')
+        print(f'No services matching {uuidVal} found.')
     else:
         printB.printServices(service_matches)
 
@@ -37,7 +40,7 @@ def scanNameService(nameVal, addr = None):
     service_matches = bluetooth.find_service(name = nameVal, bdaddr = addr)
 
     if len(service_matches == 0):
-        printf(f'No services matching {uuidVal} found.')
+        print(f'No services matching {uuidVal} found.')
     else:
         printB.printServices(service_matches)
 
@@ -50,3 +53,74 @@ def getMan(macAddr):
     for index, data in df.iterrows():
         if tempAddr.upper().startswith(data['Assignment']):
             print(data['Organization Name'])
+
+def continousScan(timeoutseconds = 10, csvlocation = "/tmp/Blueberry-DiscoveredDevices.csv"):
+    try:
+        ''' File I/O'''
+        CSVfile = open(csvlocation, 'w+')
+        file = csv.writer(CSVfile)
+
+        ''' Init local vars'''
+        printed = []
+        table = prettytable.PrettyTable(["MAC Address", "Device Name"])
+
+        print(f'Scan will automatically end in {timeoutseconds} seconds')
+        print("Press CTRL+C (Keyboard Interrupt) to end scan early")
+        signal.signal(signal.SIGALRM, timeouthandler) #raises a SIGALRM then calls fuction timeouthandler
+        signal.setitimer(signal.ITIMER_REAL, timeoutseconds) #second param is timer in seconds
+
+        while True:
+            results = bluetooth.find_service(name=None, uuid=None, address=None)
+
+            for i in results:
+                host = i['host']
+                name = i['name']
+                serviceClass = i["service-classes"]
+                profiles = i["profiles"]
+                description = i['description']
+                provider = i['provider']
+                serviceID = i['service-id']
+                protocol = i['protocol']
+                port = i['port']
+                file.writerow([host, name, serviceClass, profiles, description, provider, serviceID, protocol, port])
+
+            for i in results:
+                if i['host'] not in printed:
+                    host = i['host']
+                    name = i['name']
+                    table.add_row([host, name])
+                    print(f'Discovered:', host)
+                    printed.append(host)
+
+
+    except KeyboardInterrupt:
+        print('\nUser Interrupt Detected')
+        print('Finishing Scan')
+
+        if len(printed) == 0:
+            print("No devices found")
+            exit(1)
+
+        print('Raw CSV is located: ', csvlocation)
+        CSVfile.close()
+        print(table)
+        exit(1)
+    except TimeoutError:
+        print('Time is up!')
+        print('Finishing Scan')
+
+        if len(printed) == 0:
+            print("No devices found")
+            exit(1)
+
+        print('Raw CSV is located: ', csvlocation)
+        CSVfile.close()
+        print(table)
+        exit(1)
+
+""" Used to send an TimeoutError exception to be caught by continousScan """
+def timeouthandler(signum, frame):
+    raise TimeoutError()
+
+
+continousScan()
